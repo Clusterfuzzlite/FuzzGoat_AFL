0910f4ca23c4513322773956cab5ea26e0e76e30
syedafazilathjahaa <104976934+syedafazilathjahaa@users.noreply.github.com>
2022-05-17 14:58:47 +0530

    Update and rename build.bash to build_afl.bash

--- .clusterfuzzlite/build.bash
+++
@@ -1,8 +0,0 @@
-# Build afl-fuzz and FuzzingEngine.a
-./build_afl.bash
-# Compile target using ASan, coverage instrumentation, and link against FuzzingEngine.a
-$CXX -fsanitize=address -fsanitize-coverage=trace-pc-guard fuzzer.c FuzzingEngine.a -o fuzzer
-# Test out the build by fuzzing it. INPUT_CORPUS is a directory containing files. Ctrl-C when done.
-AFL_SKIP_CPUFREQ=1 ./afl-fuzz -i $INPUT_CORPUS -o output -m none ./fuzzer
-# Create a fuzzer build to upload to ClusterFuzz.
-zip fuzzer-build.zip fuzzer afl-fuzz afl-showmap
